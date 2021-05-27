e67a9bd42a7339b965506c43f4fb382032ce9b2c
Camille Morin <dewavrin.camille@gmail.com>
2021-05-27 14:46:12 +0200

    index filtered with status

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -3,4 +3,11 @@ class AppliesController < ApplicationController
     @apply = Apply.find(params[:id])
     @offer = @apply.offer
   end
+
+  def index
+    @applies = current_user.applies
+    if params.dig(:status)
+      @applies = @applies.where(status: params[:status])
+    end
+  end
 end
