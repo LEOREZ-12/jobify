f3c0a7885d3066c27fc92146ee46f4a81796ba06
Leo Rezvani <rezvanileo@gmail.com>
2021-05-28 11:47:54 +0200

    show work pending

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,5 @@
 class Step < ApplicationRecord
   belongs_to :apply
+
+  NAMES = ['A postuler', 'Candidature envoyée', 'Echange téléphonique', 'Entretien', 'Proposition à recevoir', 'Refus du recruteur', 'Refus du candidat', 'Proposition acceptée']
 end
