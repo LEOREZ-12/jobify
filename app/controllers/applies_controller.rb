ea51e7c40b33653cee2fbfd23a08240474089ef8
Camille Morin <dewavrin.camille@gmail.com>
2021-05-31 14:36:03 +0200

    dashboard new structure

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -6,15 +6,20 @@ class AppliesController < ApplicationController
   end
 
   def index
-    @applies = current_user.applies
-    if params.dig(:status)
-      @applies = @applies.where(status: params[:status])
-    end
-    @applies_user = current_user.applies
-    @candidatures = Apply.where(user: current_user)
+    @allapplies_user = current_user.applies
     @entretiens = Apply.where(user: current_user && "status" == "Entretien")
-    @entretiens = @applies_user.where(status: "Entretien")
-    @candidatures = Apply.where(status: "A postuler")
+    @entretiens = @allapplies_user.where(status: "Entretien")
+    @candidatures = @allapplies_user.where(status: "A postuler")
+    @propositions = @allapplies_user.where(status: "Proposition à recevoir")
+    @refusees = @allapplies_user.where("status LIKE :prefix", prefix: "Refus%")
+    @allexceptdeclined = @allapplies_user.where("status NOT LIKE :prefix", prefix: "Refus%")
+    @reminders = @allexceptdeclined.where("DATE(updated_at) >= ?", Date.today - 7.day)
+    @applies = current_user.applies
+    if params.dig(:status) == "reminder"
+      @applies = @allexceptdeclined.where("DATE(updated_at) >= ?", Date.today - 7.day)
+    elsif params.dig(:status)
+      @applies = @applies.where("status LIKE :prefix", prefix: "#{params[:status]}%")
+    end
   end
-
 end
+
