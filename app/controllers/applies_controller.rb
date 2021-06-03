c8f31e391a54c58d43e62adba0b9cf9049febbb1
Leo Rezvani <rezvanileo@gmail.com>
2021-06-03 16:49:49 +0200

    done

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -15,7 +15,7 @@ class AppliesController < ApplicationController
     @propositions = @allapplies_user.where(status: "Proposition à recevoir")
     @refusees = @allapplies_user.where("status LIKE :prefix", prefix: "Refus%")
     @allexceptdeclined = @allapplies_user.where("status NOT LIKE :prefix", prefix: "Refus%")
-    @reminders = @allexceptdeclined.where("DATE(updated_at) >= ?", Date.today - 7.day)
+    @reminders = @allexceptdeclined.where("DATE(updated_at) <= ?", Date.today - 7.day)
     @applies = current_user.applies
 
     @applies_for_fullcalendar = @applies.map { |apply| apply.content_for_fullcalendar }
