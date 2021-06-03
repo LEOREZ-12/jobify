e2adf64a9fe8d17b6a13aaeb1825a57e4fb27e23
Camille Morin <dewavrin.camille@gmail.com>
2021-06-03 11:44:48 +0200

    csschart

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,6 +32,8 @@ import { growTextarea } from '../components/growTextarea';
 import { initFlatpickr } from "../plugins/flatpickr";
 import { initCalendar } from "../plugins/init_calendar";
 
+Chartkick.configure({ language: "fr" });
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
